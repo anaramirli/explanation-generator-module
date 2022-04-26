6610c14d529efedf8bdb656a16c48cb2e54497a9
Philipp Aaron Becker <philipp.becker@dezem.de>
2022-04-26 16:54:45 +0200

    add endpoint to upload model files

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@
 
 __version__ = '1.0.0'
 
-from typing import Dict, List, Tuple
-from fastapi import FastAPI
+from multiprocessing.dummy import shutdown
+import shutil
+from typing import Dict, List
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 
 import shap
-import sys
 import joblib
-import keras 
+import keras
 import numpy as np
 import pandas as pd
 import os
@@ -28,16 +29,16 @@ class ModelPath(BaseModel):
     '''paths body for saves'''
     model: str
     scaler: str
-        
-        
+
+
 class MultivariateTimeSeriesData(BaseModel):
     '''data body provided for handling model for MultiVariateTimeseriesData'''
     data: Dict[str, List[float]]
-        
+
 
 class FeatureExplanationInput(BaseModel):
     '''request body provided for explanation generation'''
-    
+
     paths: ModelPath
     explain_data: MultivariateTimeSeriesData
     baseline_data: MultivariateTimeSeriesData
@@ -45,47 +46,47 @@ class FeatureExplanationInput(BaseModel):
     timestep_start: int
     timestep_end: int
     first_n_attribution: int = 5
-    
-    
+
+
 class FeatureExplanationOutput(BaseModel):
     '''body for shapley values of features'''
     attributing_features: Dict[str, float]
     offseting_features: Dict[str, float]
-        
 
-@app.post('/shap-kernel-explainer-keraslstm', response_model = FeatureExplanationOutput)
+
+@app.post('/shap-kernel-explainer-keraslstm', response_model=FeatureExplanationOutput)
 async def extract_important_features(mvts_data: FeatureExplanationInput):
 
     # get model
-    path_to_model = os.path.join('data',mvts_data.paths.model)
+    path_to_model = os.path.join('data', mvts_data.paths.model)
     model = keras.models.load_model(path_to_model)
 
     # get scaler
-    path_to_scaler = os.path.join('data',mvts_data.paths.scaler)
+    path_to_scaler = os.path.join('data', mvts_data.paths.scaler)
     scaler = joblib.load(path_to_scaler)
-    
-    
+
     # load and preprocess inputX and baselineX
     inputX = pd.DataFrame.from_dict(mvts_data.explain_data.data)
-    col_names = inputX.columns # get the columm names from the data
+    col_names = inputX.columns  # get the columm names from the data
     inputX = scaler.transform(inputX.values)
-    
+
     baselineX = pd.DataFrame.from_dict(mvts_data.baseline_data.data)
     baselineX = scaler.transform(baselineX.values)
-    
+
     # wrapper function (mean: as the loss of lstm model)
-    f = lambda x: np.mean(np.abs(x.reshape(x.shape[0], 1, x.shape[1]) \
-        - model.predict(x.reshape(x.shape[0], 1, x.shape[1])))**2, axis=2)
-    
+    def f(x): return np.mean(np.abs(x.reshape(x.shape[0], 1, x.shape[1])
+                                    - model.predict(x.reshape(x.shape[0], 1, x.shape[1])))**2, axis=2)
+
     # initialize explainer
     explainer = shap.KernelExplainer(f, baselineX)
-    
+
     # calculate shapley values for selected timesteps
     if mvts_data.allsteps_included == True:
         shap_values = explainer.shap_values(inputX)
     else:
         try:
-            shap_values = explainer.shap_values(inputX[mvts_data.timestep_start:mvts_data.timestep_end])
+            shap_values = explainer.shap_values(
+                inputX[mvts_data.timestep_start:mvts_data.timestep_end])
         except IndexError:
             "Timestep is out of range."
 
@@ -93,28 +94,52 @@ async def extract_important_features(mvts_data: FeatureExplanationInput):
     assert mvts_data.first_n_attribution <= inputX.shape[1], "first n is out of data range"
 
     # get mean value of Shapley values for each feature
-    feature_importance = np.mean(shap_values[0],axis=0)
-    
+    feature_importance = np.mean(shap_values[0], axis=0)
+
     # get mean value of Shapley values for each feature, and sort them
-    feature_importance = np.mean(shap_values[0],axis=0)
+    feature_importance = np.mean(shap_values[0], axis=0)
     feature_importance = dict(list(zip(col_names, feature_importance)))
-    feature_importance = sorted(feature_importance.items(), key=lambda item: item[1], reverse=True)
+    feature_importance = sorted(
+        feature_importance.items(), key=lambda item: item[1], reverse=True)
 
     # get top n attributing and offsetting features
     top_n = mvts_data.first_n_attribution
-    attributing = [{key:round(value, 4)} for key, value in feature_importance if value>0][:top_n]
-    offseting = [{key:round(value, 4)} for key, value in feature_importance if value<0][-top_n:]
-    
+    attributing = [{key: round(value, 4)} for key,
+                   value in feature_importance if value > 0][:top_n]
+    offseting = [{key: round(value, 4)} for key,
+                 value in feature_importance if value < 0][-top_n:]
+
     # cover attributing and offsetting dictlists to dict
     attributing_top = {}
     for element in attributing:
         dict_elements = list(element.items())[0]
         attributing_top[dict_elements[0]] = dict_elements[1]
-        
+
     offseting_top = {}
     for element in offseting[::-1]:
         dict_elements = list(element.items())[0]
         offseting_top[dict_elements[0]] = dict_elements[1]
-        
+
     return FeatureExplanationOutput(attributing_features=attributing_top,
-                               offseting_features=offseting_top)
+                                    offseting_features=offseting_top)
+
+
+@app.post("/upload-model/")
+async def upload_model(file: UploadFile = File(...)):
+    file_location = os.path.join('data', file.filename)
+    print(file_location)
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+
+    extract_dir = file_location[:-4]
+
+    if file_location[-4:] == '.zip':
+
+        # if os.path.exists(extract_dir):
+        #     shutil.rmtree(extract_dir)
+        # os.makedirs(extract_dir)
+
+        shutil.unpack_archive(file_location, extract_dir, format='zip')
+        os.remove(file_location)
+
+    return {"filename": file.filename}
